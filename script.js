3444e0254d1981c615a70651ea179cef5f34f010
RitousT <celestiallight.dmv@gmail.com>
2024-10-26 13:57:10 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Interaction for "Notify Me" button
+document.querySelector('.notify-button').addEventListener('click', () => {
+    alert('Thank you for your interest! We will keep you updated on our launch.');
+});
